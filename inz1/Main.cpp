bc83c4511d75e9cb88695a49a0eeaad479f1d943
tof3r <dawidg90@gmail.com>
2013-02-20 14:29:41 +0100

    obraz roznicowy v0.1

--- inz1/Main.cpp
+++ inz1/Main.cpp
@@ -11,7 +11,9 @@ int main(int argc, char** argv)
 
 	if(!z_kamerki.isOpened()) return -1;
 
-	Mat ramka, ramka_gs, ramka_binary;
+	Mat ramka, ramka_gs, ramka_poprzednia, roznica, binary;
+
+	bool pierwsza_ramka = true;
 
 	while(true){
 		/*pobranie kolejnej ramki z kamery*/
@@ -20,17 +22,32 @@ int main(int argc, char** argv)
 		/*ka¿da klatka z kamery jest zamieniona na odcieñ szaroœci*/
 		cvtColor(ramka,ramka_gs,CV_BGR2GRAY);
 
-		/*progowanie obrazu w skali szaroœci do binarnego*/
-		ramka_binary = ramka_gs > 50;
+		/*zapamiêtanie pierwszej ramki, do dalszych porównañ*/
+		if(pierwsza_ramka){
+			ramka_poprzednia = ramka_gs.clone();
+			pierwsza_ramka = false;
+			continue;
+		}
+
+		absdiff(ramka_gs, ramka_poprzednia, roznica);
+
+		threshold(roznica,binary, 50, 255,THRESH_BINARY);
+
+		erode(binary, binary,Mat(),Point(-1,-1),1);
+		dilate(binary, binary,Mat(),Point(-1,-1),1);
 	
 		imshow("Z kamery",ramka);
 		imshow("Greyscale",ramka_gs);
-		imshow("Binary",ramka_binary);
+		imshow("Binary",binary);
+
+		ramka_poprzednia=ramka_gs.clone();
 
 		int listener = cv::waitKey(40);
 
 		if(listener == 27)break;
 	}
+
+	z_kamerki.release();
 
 	return 0;
 }
